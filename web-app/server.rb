d0b25091e569f819cf90b552692c0ce091b625b0
David Ma <davidma415@gmail.com>
2018-08-01 10:20:32 -0400

    Finished favourites

--- web-app/server.rb
+++ web-app/server.rb
@@ -13,3 +13,8 @@ get '/about_me' do
   @interests = ['basketball', 'numbers', 'music']
   erb :about
 end
+
+get '/favourites' do
+  @fav_links = ['http://youtube.com', 'https://www.theplayerstribune.com/en-us','http://www.speedhunters.com/']
+  erb :favourites
+end
